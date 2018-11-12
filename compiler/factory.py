d99b6828dd0344226c19be6c5b51192c0dc7c256
erickpeirson <brp53@cornell.edu>
2018-11-12 05:46:36 -0500

    error responses should be JSON

--- compiler/factory.py
+++ compiler/factory.py
@@ -2,7 +2,10 @@
 
 import logging
 
-from flask import Flask
+from werkzeug.exceptions import Forbidden, Unauthorized, NotFound, \
+    InternalServerError, BadRequest
+
+from flask import Flask, jsonify
 
 from arxiv.base import Base
 from arxiv.users import auth
@@ -10,6 +13,14 @@ from arxiv.base.middleware import wrap, request_logs
 
 from .services import filemanager, store
 from . import routes
+
+
+def jsonify_exception(error):
+    """Render the base 404 error page."""
+    exc_resp = error.get_response()
+    response = jsonify(reason=error.description)
+    response.status_code = exc_resp.status_code
+    return response
 
 
 def create_app() -> Flask:
@@ -24,6 +35,11 @@ def create_app() -> Flask:
 
     # Don't forget to register your API blueprint, when it's ready.
     app.register_blueprint(routes.blueprint)
+    app.errorhandler(Forbidden)(jsonify_exception)
+    app.errorhandler(Unauthorized)(jsonify_exception)
+    app.errorhandler(BadRequest)(jsonify_exception)
+    app.errorhandler(InternalServerError)(jsonify_exception)
+    app.errorhandler(NotFound)(jsonify_exception)
 
     wrap(app, [auth.middleware.AuthMiddleware])
 
