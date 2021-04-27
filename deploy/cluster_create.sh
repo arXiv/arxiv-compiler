be2718defbed4c0fb06add99619a2eee69793fb6
David Fielding <dlf2@cornell.edu>
2021-04-27 08:16:22 -0400

    Added parameters to cluster create. [ARXIVNG-3742] David

--- deploy/cluster_create.sh
+++ deploy/cluster_create.sh
@@ -16,14 +16,29 @@ else
     echo "Config file '$CONFIG' not found"
 fi
 
+CLUSTER_TYPE=zone
+CLUSTER_VALUE=$CLUSTER_ZONE
+
 exists=$(gcloud container clusters list --filter=$CLUSTER)
 
-if [ $exists ]; then
+if [ "$exists" ]; then
     echo "ERROR: Cluster '$CLUSTER' already exists!"
-    gcloud container clusters list
+    gcloud container clusters list --$CLUSTER_TYPE=$CLUSTER_VALUE
     echo "Aborting create cluster request. Delete cluster first!"
 else
     echo "Creating cluster $CLUSTER now!"
-    gcloud container clusters create $CLUSTER --region=$CLUSTER_REGION --enable-ip-alias --num-nodes=$CLUSTER_NODES
+    echo "   Type is $CLUSTER_TYPE and Size is $CLUSTER_NODES"
+    gcloud container clusters create $CLUSTER \
+      --disk-size "100" \
+      --disk-type "pd-standard" \
+      --enable-autorepair \
+      --enable-autoupgrade \
+      --enable-ip-alias \
+      --machine-type "e2-medium" \
+      --num-nodes=$CLUSTER_NODES \
+      --$CLUSTER_TYPE=$CLUSTER_VALUE
+      --release-channel "None" \
+      --service-account "arxiv-compiler-dev@appspot.gserviceaccount.com"
+
     gcloud container clusters list
 fi
